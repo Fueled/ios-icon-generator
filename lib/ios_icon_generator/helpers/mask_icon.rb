0f14170b3420cadf31720eba8f666989ca19ba73
Stéphane Copin <stephane@fueled.com>
2019-02-20 14:52:58 -0500

    fix(mask): fix issue where the mask wouldn't write the resulting Contents.json file

--- lib/ios_icon_generator/helpers/mask_icon.rb
+++ lib/ios_icon_generator/helpers/mask_icon.rb
@@ -35,13 +35,13 @@ module IOSIconGenerator
       contents_path = File.join(appiconset_path, 'Contents.json')
       raise "Contents.json file not found in #{appiconset_path}" unless File.exist?(contents_path)
 
-      images = JSON.parse(File.read(contents_path))['images']
-      progress&.call(nil, images.count)
+      json_content = JSON.parse(File.read(contents_path))
+      progress&.call(nil, json_content['images'].count)
       Parallel.each(
-        images,
+        json_content['images'],
         in_processes: parallel_processes,
         finish: lambda do |_item, i, _result|
-          progress&.call(i, images.count)
+          progress&.call(i, json_content['images'].count)
         end
       ) do |image|
         width, height = /(\d+(?:\.\d)?)x(\d+(?:\.\d)?)/.match(image['size'])&.captures
@@ -59,13 +59,13 @@ module IOSIconGenerator
         icon_output = "#{File.basename(image['filename'], extension)}-#{mask[:suffix]}#{extension}"
         icon_output_path = File.join(output_folder, icon_output)
 
-        draw_shape_parameters = "-strokewidth #{(mask[:stroke_width_offset] || 0) * [width, height].min} -stroke '#{mask[:stroke_color] || '#000000'}' -fill '#{mask[:background_color] || '#FFFFFF'}'"
+        draw_shape_parameters = "-strokewidth '#{(mask[:stroke_width_offset] || 0) * [width, height].min}' -stroke '#{mask[:stroke_width_offset].zero? ? 'none' : (mask[:stroke_color] || '#000000')}' -fill '#{mask[:background_color] || '#FFFFFF'}'"
         draw_shape =
           case mask[:shape]
           when :triangle
             "-draw \"polyline 0,#{height - mask_size_height} 0,#{height} #{mask_size_width},#{height}\""
           when :square
-            "-draw \"rectangle 0,#{height} #{height - mask_size_height},#{mask_size_width}\""
+            "-draw \"rectangle -#{width},#{height * 2.0} #{height - mask_size_height},#{mask_size_width}\""
           else
             raise "Unknown mask shape: #{mask[:shape]}"
           end
@@ -74,12 +74,14 @@ module IOSIconGenerator
           if mask[:file]
             "\\( -background none -density 1536 -resize #{width * mask[:size_offset]}x#{height} \"#{mask[:file]}\" -geometry +#{width * mask[:x_offset]}+#{height * mask[:y_offset]} \\) -gravity southwest -composite"
           else
-            " -strokewidth 0 -stroke none -fill '#{mask[:symbol_color] || '#7F0000'}' -font '#{mask[:font]}' -pointsize #{height * mask[:size_offset] * 2.0} -annotate +#{width * mask[:x_offset]}+#{height - height * mask[:y_offset]} '#{mask[:symbol]}'"
+            "-strokewidth 0 -stroke none -fill '#{mask[:symbol_color] || '#7F0000'}' -font '#{mask[:font]}' -pointsize #{height * mask[:size_offset] * 2.0} -annotate +#{width * mask[:x_offset]}+#{height - height * mask[:y_offset]} '#{mask[:symbol]}'"
           end
         system("convert '#{File.join(appiconset_path, image['filename'])}' #{draw_shape_parameters} #{draw_shape} #{draw_symbol} '#{icon_output_path}'")
 
         image['filename'] = icon_output
       end
+
+      File.write(File.join(output_folder, 'Contents.json'), JSON.generate(json_content))
     end
   end
 end

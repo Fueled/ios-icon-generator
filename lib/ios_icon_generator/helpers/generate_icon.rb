a6c7da30b961ea66705ada0bf6cd6f72a2248963
Stéphane Copin <stephane@fueled.com>
2019-02-20 18:00:02 -0500

    docs(license): add Apache 2.0 license

--- lib/ios_icon_generator/helpers/generate_icon.rb
+++ lib/ios_icon_generator/helpers/generate_icon.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# Copyright (c) 2019 Fueled Digital Media, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require 'json'
 require 'fileutils'
 require 'ios_icon_generator/helpers/images_sets_definition'
@@ -18,7 +32,7 @@ module IOSIconGenerator
         width, height = matches.captures
         raise 'Invalid pdf specified.' if width.nil? || height.nil?
 
-        raise "The icon must at least be 1024x1024, it currently is #{width}x#{height}." unless width.to_i >= 1024 && height.to_i >= 1024  
+        raise "The icon must at least be 1024x1024, it currently is #{width}x#{height}." unless width.to_i >= 1024 && height.to_i >= 1024
       end
       appiconset_path = File.join(output_folder, "#{types.include?(:imessage) ? 'iMessage App Icon' : 'AppIcon'}.#{types.include?(:imessage) ? 'stickersiconset' : 'appiconset'}")
 
